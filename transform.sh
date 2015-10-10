2586cd09a95b39f2302fff3758991771256cb900
Oliver Newland <oliver.newland@crowdflower.com>
2015-10-10 10:57:39 -0700

    parameterize

--- transform.sh
+++ transform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in snap1 snap2 snap3 snap4; do
+for i in $1 $2 $3 $4; do
 	convert $i.jpg -colorspace gray -sigmoidal-contrast 10,40% $i-gray.jpg;
 	convert $i-gray.jpg -crop 330x720+445+0 $i-crop.jpg;
 	convert $i-crop.jpg -resize 480x1080\! $i-crop-resized.jpg;
@@ -8,10 +8,11 @@ done
 
 montage -geometry 480x1080 \
         -tile 4x1 \
-	snap1-crop-resized.jpg snap2-crop-resized.jpg snap3-crop-resized.jpg snap4-crop-resized.jpg \
+	$1-crop-resized.jpg $2-crop-resized.jpg $3-crop-resized.jpg $4-crop-resized.jpg \
 	snapshot-montage.jpg
 
-for i in snap1 snap2 snap3 snap4; do
-	rm $i.jpg;
+for i in $1 $2 $3 $4; do
+	rm $i-gray.jpg;
 	rm $i-crop.jpg;
+	rm $i-crop-resized.jpg;
 done
