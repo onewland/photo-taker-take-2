51f25be55b3e9a965aff3ee4cc568cb2a1822f4f
Oliver Newland <oliver.newland@crowdflower.com>
2015-10-10 12:50:18 -0700

    load images from photos

--- eat_the_rich.py
+++ eat_the_rich.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 import boto3
+import random
+import glob
 
 QUEUE_URL = "https://queue.amazonaws.com/829315242464/button_events"
 sqs = boto3.client("sqs")
@@ -14,7 +16,8 @@ red = pygame.Color(255,50,50)
 white = pygame.Color(255,255,255)
 black = pygame.Color(0,0,0)
 font = pygame.font.Font(None, 256)
-text = font.render("EAT THE RICH", 1, white, red)
+imgPath = 'photos/6fd46727e9df35668c6e1235287e3d6e.jpg'
+text = font.render("TEST TRANSMISSION", 1, white, red)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -22,13 +25,19 @@ GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 screen = pygame.display.set_mode([0,0], pygame.FULLSCREEN)
 pygame.mouse.set_visible(False)
 
+def randomImagePath():
+    return random.choice(glob.glob('photos/*.jpg'))
+
 while 1:
     GPIO.wait_for_edge(channel,GPIO.FALLING)
-    screen.fill(red)
-    screen.blit(text, (100,100))
+    screen.fill(black)
+    myimage = pygame.image.load(randomImagePath())
+    imagerect = myimage.get_rect()
+    screen.blit(myimage, imagerect)
     pygame.display.update()
+
     GPIO.wait_for_edge(channel,GPIO.RISING)
     sqs.send_message(QueueUrl=QUEUE_URL,MessageBody=unicode(datetime.datetime.now()))
-    time.sleep(1)
-    screen.fill(black)
+    screen.fill(red)
+    screen.blit(text, (100,100))
     pygame.display.update()
