14f130382e6282a20099c87c475bf71a5ad12a24
Oliver <onewland@gmail.com>
2015-10-10 09:44:47 -0700

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+listener: python poller.py
+photo-taker: imagesnap -w 1 -t 1.0
---
+++ poller.py
@@ -0,0 +1,19 @@
+import boto3
+import subprocess
+import datetime
+
+QUEUE_URL = "https://queue.amazonaws.com/829315242464/button_events"
+last_deletion = datetime.datetime.now()
+sqs = boto3.client("sqs")
+
+while True:
+    response = sqs.receive_message(QueueUrl=QUEUE_URL, WaitTimeSeconds=20)
+
+    if "Messages" in response:
+        message = response["Messages"][0]
+        print message
+
+        del_response = sqs.delete_message(QueueUrl=QUEUE_URL, ReceiptHandle=message['ReceiptHandle'])
+
+    else:
+        print "No messages"
