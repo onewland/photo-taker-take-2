34b3e7befe259c187837ff8549f86d84f43d2bf6
Oliver Newland <oliver.newland@crowdflower.com>
2015-10-10 18:29:18 -0700

    mo text

--- eat_the_rich.py
+++ eat_the_rich.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import boto3
 import random
+import os
 import glob
 
 QUEUE_URL = "https://queue.amazonaws.com/829315242464/button_events"
@@ -17,7 +18,12 @@ white = pygame.Color(255,255,255)
 black = pygame.Color(0,0,0)
 font = pygame.font.Font(None, 192)
 imgPath = 'photos/6fd46727e9df35668c6e1235287e3d6e.jpg'
-text = font.render("TRANSGRESSION RECORDED", 1, white, red)
+textTransgression = font.render("TRANSGRESSION", 1, white, red)
+textRecorded = font.render("RECORDED", 1, white, red)
+
+fontSmaller = pygame.font.Font(None, 72)
+propagandaText = fontSmaller.render("WE NEED YOUR HELP TO ERADICATE BUTTON-PUSHERS, CITIZEN.", 1, white)
+
 last_pressed = time.time()
 last_image_switch = time.time()
 IMAGE_SWITCH_SECONDS = 60
@@ -33,34 +39,48 @@ GPIO.add_event_detect(channel, GPIO.RISING)
 def randomImagePath():
     return random.choice(glob.glob('photos/*.jpg'))
 
-current_image = pygame.image.load(randomImagePath())
+current_image_filename = randomImagePath()
+current_image = pygame.image.load(current_image_filename)
 
 def maybe_image_render():
-    global last_image_switch, current_image
+    global last_image_switch, current_image, currnet_image_filename
 
     now = time.time()
     if now - last_image_switch > IMAGE_SWITCH_SECONDS:
-        current_image = pygame.image.load(randomImagePath())
+        current_image_filename = randomImagePath()
+        current_image = pygame.image.load(current_image_filename)
         render_current_image()
         last_image_switch = now
+
+def md5_from_filename(fname):
+    return os.path.splitext(os.path.split(fname)[1])[0]
 
 def render_current_image():
     imagerect = current_image.get_rect()
     screen.blit(current_image, imagerect)
+
+    rect = pygame.Surface((1920,200), pygame.SRCALPHA, 32)
+    rect.fill((0, 0, 0, 200))
+    rect.blit(propagandaText, (80,50))
+    screen.blit(rect, (0,0))
+
+    filenameText = fontSmaller.render("violator-id-" + md5_from_filename(current_image_filename), 1, white)
+    screen.blit(filenameText, (80, 850))
+
     pygame.display.update()
 
 render_current_image()
+
 while 1:
-    # GPIO.wait_for_edge(channel,GPIO.FALLING)
     maybe_image_render()
-    # GPIO.wait_for_edge(channel,GPIO.RISING)
     if(GPIO.event_detected(channel)):
         now = time.time()
         if now - last_pressed > 3:
             sqs.send_message(QueueUrl=QUEUE_URL,MessageBody=unicode(datetime.datetime.now()))
             last_pressed = now
             screen.fill(red)
-            screen.blit(text, (100,100))
+            screen.blit(textTransgression, (100,100))
+            screen.blit(textRecorded, (100,300))
             pygame.display.update()
             time.sleep(1)
             render_current_image()
